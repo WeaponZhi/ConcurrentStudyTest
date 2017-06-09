0b2059e4c9a4c51ba21c3e252093be6099a7a361
WeaponZhi <584098488@qq.com>
2017-06-09 18:42:01 +0800

    ThreadLoacl 注释完善

--- src/Profiler.java
+++ src/Profiler.java
@@ -1,11 +1,13 @@
 /**
  * Profiler ThreadLocal的使用
+ * 这个类可以被复用在方法调用耗时统计的功能上。好处是这两个方法的调用不用在一个方法或者类中。
  * author:张冠之
  * time: 2017/6/9 16:02
  * e-mail: guanzhi.zhang@sojex.cn
  */
 
 class Profiler {
+    //第一次 get() 方法调用时会初始化（如果 set 方法没有调用），每个线程会调用一次。
     private static final ThreadLocal<Long> TIME_THREADLOCAL = ThreadLocal.withInitial(() -> System.currentTimeMillis());
 
     public static final void begin(){
@@ -19,6 +21,6 @@ class Profiler {
     public static void main(String[] args) {
         Profiler.begin();
         SleepUtils.second(1);
-        System.out.println("Cost: "+ Profiler.end() + "mills");
+        System.out.println("Cost: "+ Profiler.end() + "　mills");
     }
 }
